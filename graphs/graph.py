fba020bcba829b1bff6f7979b72c6fc0ebbcc5f6
Mike Beaumier <michael.beaumier@gmail.com>
2020-01-19 13:21:16 -0800

    latest

--- graphs/graph.py
+++ graphs/graph.py
@@ -9,8 +9,22 @@ class Graph(object):
         else:
             self.graph_dict = copy.deepcopy(graph_dict)
 
-    def add_node(self, node, attrs):
+    def add_node(self, node_id, data=None, connections=[]):
         if node not in self.graph_dict:
             self.graph_dict
 
+
+print(hash(lambda x: x - 1))
+print(hash(lambda x: x - 1))
+
+g1 = Graph()
+g2 = Graph()
+
+
+print(hash(g1))
+print(hash(g2))
+print(hash(-1))
+ 
+val = -1
+print(hash(val))
 
